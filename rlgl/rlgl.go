1c5ff469a0f97581567b75e9a1ee38254fe2ee7d
Anthony Green <green@moxielogic.com>
2019-01-15 08:01:09 -0500

    Require a policy on evaluation

--- rlgl/rlgl.go
+++ rlgl/rlgl.go
@@ -1,124 +1,129 @@
 package main
 
 import (
-  "bytes"
-  "encoding/json"
-  "fmt"
-  "log"
-  "os"
-  "time"
-  "io/ioutil"
-  "net/http"
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"time"
 
-  "github.com/urfave/cli"
+	"github.com/urfave/cli"
 )
 
 func main() {
-  var policy string
+	var policy string
 
-  app := cli.NewApp()
+	app := cli.NewApp()
 
-  app.Commands = []cli.Command{
-    {
-      Name:    "login",
-      Aliases: []string{"l"},
-      Usage:   "login to Red Light Green Light server",
-      Action:  func(c *cli.Context) error {
-        fmt.Println("login task: ", c.Args().First())
-        return nil
-      },
-    },
-    {
-      Name:    "start",
-      Aliases: []string{"s"},
-      Usage:   "create a Player ID",
-      Action:  func(c *cli.Context) error {
-
-  	response, err := http.Get("https://rlgl-server-rlgl.apps.home.labdroid.net/start")
-
-	if err != nil {
-		fmt.Print(err.Error())
-		os.Exit(1)
-	}
-	
-	responseData, err := ioutil.ReadAll(response.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println(string(responseData))
-
-        return nil
-      },
-    },
-    {
-      Name:    "evaluate",
-      Aliases: []string{"e"},
-      Usage:   "evaluate test results",
-      Flags: []cli.Flag{
-        cli.StringFlag{
-		Name: "policy",
-		Value: "default",
-		Usage: "evaluation policy",
-		Destination: &policy,
+	app.Commands = []cli.Command{
+		{
+			Name:    "login",
+			Aliases: []string{"l"},
+			Usage:   "login to Red Light Green Light server",
+			Action: func(c *cli.Context) error {
+				fmt.Println("login task: ", c.Args().First())
+				return nil
+			},
 		},
-      },
+		{
+			Name:    "start",
+			Aliases: []string{"s"},
+			Usage:   "create a Player ID",
+			Action: func(c *cli.Context) error {
 
-      Action:  func(c *cli.Context) error {
+				response, err := http.Get("https://rlgl-server-rlgl.apps.home.labdroid.net/start")
 
-      if c.NArg() == 0 {
-            fmt.Print("ERROR: Missing report\n")
-      	    return nil
-	 }
+				if err != nil {
+					fmt.Print(err.Error())
+					os.Exit(1)
+				}
 
-      file, err := os.Open(c.Args().Get(0));
-      defer file.Close();
+				responseData, err := ioutil.ReadAll(response.Body)
+				if err != nil {
+					log.Fatal(err)
+				}
+				fmt.Println(string(responseData))
 
-      res, err := http.Post("https://rlgl-server-rlgl.apps.home.labdroid.net/upload", "application/octet-stream", file);
-      if err != nil {
-      	 // panic(err)
-	 }
-	 defer res.Body.Close();
-	 message, _ := ioutil.ReadAll(res.Body)
-	 // check that it is OK?
+				return nil
+			},
+		},
+		{
+			Name:    "evaluate",
+			Aliases: []string{"e"},
+			Usage:   "evaluate test results",
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:        "policy",
+					Value:       "",
+					Usage:       "evaluation policy",
+					Destination: &policy,
+				},
+			},
 
-          values := map[string]string{"name": file.Name(), "ref": string(message[:])}
-	  jsonValue, _ := json.Marshal(values)
+			Action: func(c *cli.Context) error {
 
-	 response, err := http.Post("https://rlgl-server-rlgl.apps.home.labdroid.net/evaluate/", "application/json", bytes.NewBuffer(jsonValue))
+				if policy == "" {
+					fmt.Print("ERROR: Missing policy\n")
+					return nil
+				}
 
-	if err != nil {
-		fmt.Print(err.Error())
-		os.Exit(1)
+				if c.NArg() == 0 {
+					fmt.Print("ERROR: Missing report\n")
+					return nil
+				}
+
+				file, err := os.Open(c.Args().Get(0))
+				defer file.Close()
+
+				res, err := http.Post("https://rlgl-server-rlgl.apps.home.labdroid.net/upload", "application/octet-stream", file)
+				if err != nil {
+					// panic(err)
+				}
+				defer res.Body.Close()
+				message, _ := ioutil.ReadAll(res.Body)
+				// check that it is OK?
+
+				values := map[string]string{"policy": policy, "name": file.Name(), "ref": string(message[:])}
+				jsonValue, _ := json.Marshal(values)
+
+				response, err := http.Post("https://rlgl-server-rlgl.apps.home.labdroid.net/evaluate/", "application/json", bytes.NewBuffer(jsonValue))
+
+				if err != nil {
+					fmt.Print(err.Error())
+					os.Exit(1)
+				}
+
+				responseData, err := ioutil.ReadAll(response.Body)
+				if err != nil {
+					log.Fatal(err)
+				}
+				fmt.Println(string(responseData))
+
+				return nil
+			},
+		},
 	}
-	
-	responseData, err := ioutil.ReadAll(response.Body)
+
+	app.Name = "rlgl"
+	app.Copyright = "(c) 2018, 2019 Anthony Green"
+	app.Compiled = time.Now()
+	app.Authors = []cli.Author{
+		cli.Author{
+			Name:  "Anthony Green",
+			Email: "green@moxielogic.com",
+		},
+	}
+	app.Usage = "Red Light Green Light"
+	app.Action = func(c *cli.Context) error {
+
+		return nil
+	}
+
+	err := app.Run(os.Args)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(string(responseData))
-
-        return nil
-      },
-    },
-  }
-
-  app.Name = "rlgl"
-  app.Copyright = "(c) 2018, 2019 Anthony Green"
-  app.Compiled = time.Now()
-  app.Authors = []cli.Author{
-  	  cli.Author{
-	    Name: "Anthony Green",
-	    Email: "green@moxielogic.com",
-	    },
-	    }
-  app.Usage = "Red Light Green Light"
-  app.Action = func(c *cli.Context) error {
-
-    return nil
-  }
-
-  err := app.Run(os.Args)
-  if err != nil {
-    log.Fatal(err)
-  }
 }
