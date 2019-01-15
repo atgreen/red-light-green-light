03c7e05cf00f4c8722f9d0e30d2492a53a87a364
Anthony Green <green@moxielogic.com>
2019-01-14 20:24:47 -0500

    More CSS to external file.

--- rlgl-server/rlgl-server.lisp
+++ rlgl-server/rlgl-server.lisp
@@ -87,7 +87,7 @@ server-uri = \"http://localhost:8080\"
 	  "ERROR"
 	  (let ((processed-results (apply-policy *policy* tests)))
 	    (let ((stream (make-string-output-stream)))
-	      (render stream processed-results)
+	      (render stream (cdr (assoc :REF json)) processed-results)
 	      (format nil "green: ~A/doc?id=~A~%~%"
 		      *server-uri*
 		      (store-document *storage-driver*
@@ -109,95 +109,18 @@ server-uri = \"http://localhost:8080\"
       (:doctype)
       (:html
         (:head
-         (:title ,title))
+         (:title ,title)
+	 (:link :rel "stylesheet" :href "css/rlgl.css")
+	 (:script :src "https://cdnjs.cloudflare.com/ajax/libs/prefixfree/1.0.7/prefixfree.min.js"))
         (:body ,@body))))
 
-(defun render (stream results)
+(defun render (stream report-ref results)
   (let ((*html* stream))
     (with-page (:title "Report")
-      (:header
-       (:style
-	("@import url(\"https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css\");
-
-* {
-  box-sizing: border-box;
-}
-
-body {
-  padding: .2em 2em;
-}
-
-table {
-  width: 100%;
-}
-table th {
-  text-align: left;
-  border-bottom: 1px solid #ccc;
-}
-table th, table td {
-  padding: .4em;
-}
-
-table.fold-table > tbody > tr.view td, table.fold-table > tbody > tr.view th {
-  cursor: pointer;
-}
-table.fold-table > tbody > tr.view td:first-child,
-table.fold-table > tbody > tr.view th:first-child {
-  position: relative;
-  padding-left: 20px;
-}
-table.fold-table > tbody > tr.view td:first-child:before,
-table.fold-table > tbody > tr.view th:first-child:before {
-  position: absolute;
-  top: 50%;
-  left: 5px;
-  width: 9px;
-  height: 16px;
-  margin-top: -8px;
-  font: 16px fontawesome;
-  color: #999;
-  content: \"\\f0d7\";
-  transition: all .3s ease;
-}
-table.fold-table > tbody > tr.view:nth-child(4n-1) {
-  background: #eee;
-}
-table.fold-table > tbody > tr.view:hover {
-  background: #000;
-}
-table.fold-table > tbody > tr.view.open {
-  background: tomato;
-  color: white;
-}
-table.fold-table > tbody > tr.view.open td:first-child:before, table.fold-table > tbody > tr.view.open th:first-child:before {
-  transform: rotate(-180deg);
-  color: #333;
-}
-table.fold-table > tbody > tr.fold {
-  display: none;
-}
-table.fold-table > tbody > tr.fold.open {
-  display: table-row;
-}
-
-.fold-content {
-  padding: .5em;
-}
-.fold-content h3 {
-  margin-top: 0;
-}
-.fold-content > table {
-  border: 2px solid #ccc;
-}
-.fold-content > table > tbody tr:nth-child(even) {
-  background: #eee;
-}")
-(:script :src "https://cdnjs.cloudflare.com/ajax/libs/prefixfree/1.0.7/prefixfree.min.js"))
-
-
-       (:h1 "Report"))
+      (:h1 "Report")
       (:section
-       ("This is your report")
+       (:a :href (format nil "~A/doc?id=~A" *server-uri* report-ref)
+	   :target "_blank" "Original Report")
        (:table :class "fold-table" :id "results"
 	       (:body
 		(:tr (:th "RESULT") (:th "ID"))
@@ -205,8 +128,8 @@ table.fold-table > tbody > tr.fold.open {
 		  (let ((matcher (car item))
 			(alist (cdr item)))
 		    (:tr :class "view"
-		     (:td (cdr (assoc :RESULT alist)))
-		     (:td (:a :href (cdr (assoc :URL alist)) :target "_blank" (cdr (assoc :ID alist)))))
+			 (:td (cdr (assoc :RESULT alist)))
+			 (:td (:a :href (cdr (assoc :URL alist)) :target "_blank" (cdr (assoc :ID alist)))))
 		    (:tr :class "fold"
 			 (:td :colspan "2")
 			 (:div :class "fold-content"
@@ -219,8 +142,7 @@ table.fold-table > tbody > tr.fold.open {
 				     (:pre (str:trim (format nil "~{~A~%~}" (cdr log-lines))))))
 			       (:pre (cl-json-util:pretty-json (json:encode-json-to-string alist)))))))))
        (:script :attrs (list :src "http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"))
-       (:script :attrs (list :src "js/index.js"))
-      (:footer ("All done"))))))
+       (:script :attrs (list :src "js/index.js"))))))
 	      
 ;;; Read JSON pattern ---------------------------------------------------------
 
