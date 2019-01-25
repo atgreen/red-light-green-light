03c5cc691f9c41e4187adcebae2810bdad306960
Anthony Green <green@moxielogic.com>
2019-01-25 08:21:19 -0500

    Test

--- db.lisp
+++ db.lisp
@@ -32,7 +32,8 @@
   (let ((db (dbi:connect-cached :sqlite3 :database-name *sqlite-db-filename*)))
     (when fresh
       (dbi:do-sql db "drop table log;"))
-    (let ((query (dbi:prepare db "create table if not exists log (id char(12), version char(40), result varchar(6), report varchar(24) not null, Timestamp DATETIME DEFAULT CURRENT_TIMESTAMP)")))
+    ;;    (let ((query (dbi:prepare db "create table if not exists log (id char(12), version char(40), result varchar(6), report varchar(24) not null, Timestamp DATETIME DEFAULT CURRENT_TIMESTAMP)")))
+    (let ((query (dbi:prepare db "create table if not exists log (id char(12), report varchar(24) not null, Timestamp DATETIME DEFAULT CURRENT_TIMESTAMP)")))
       (dbi:execute query))))
 
 (defun record-log (player version result report)
