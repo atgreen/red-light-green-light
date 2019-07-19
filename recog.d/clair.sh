bc975fa2a30c211dea997c326ca9ab426a07a0cd
Anthony Green <green@moxielogic.com>
2019-07-19 12:46:56 -0400

    Fix typo in clair report recognizer

--- recog.d/clair.sh
+++ recog.d/clair.sh
@@ -2,9 +2,8 @@
 
 grep -q "unapproved" $1
 if [ $? -eq 0 ]; then
-    grep -q "vulnerabilites" $1;
+    grep -q "vulnerabilities" $1;
     if [ $? -eq 0 ]; then
-# possibly test with json parser here...
 	echo "clair";
     fi
 fi
