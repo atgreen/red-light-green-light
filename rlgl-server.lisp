4b45e22edadbdd890411bbcd753aa92023ef2891
Anthony Green <green@moxielogic.com>
2019-07-30 09:57:37 -0400

    Shell needs to be one line

--- rlgl-server.lisp
+++ rlgl-server.lisp
@@ -26,9 +26,7 @@
 (eval-when (:compile-toplevel)
   (defun get-version-from-git ()
     (inferior-shell:run/ss
-     "(test -d .git && git describe --tags --dirty=+) \
-      || (test -d /tmp/src/.git && cd /tmp/src && git describe --tags --dirty=+) \
-      || echo UNKNOWN)")))
+     "(test -d .git && git describe --tags --dirty=+) || (test -d /tmp/src/.git && cd /tmp/src && git describe --tags --dirty=+) || echo UNKNOWN")))
 (defmacro rlgl-version () (get-version-from-git))
 
 ;; ----------------------------------------------------------------------------
