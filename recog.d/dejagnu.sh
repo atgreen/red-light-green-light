8cf8460fdf4569df2dafd27504e564e62aa72af2
Anthony Green <green@moxielogic.com>
2019-11-14 01:11:29 -0500

    Recognizers should always exit with 0

--- recog.d/dejagnu.sh
+++ recog.d/dejagnu.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-grep -q "Test [Rr]un [Bb]y " $1 \
-    && grep -q "Schedule of variations" $1 \
-	    && echo "dejagnu";
+(grep -q "Test [Rr]un [Bb]y " $1 \
+  && grep -q "Schedule of variations" $1 \
+  && echo "dejagnu") \
+ || true;
