a87f35f3df8e2694578c97e60b55babd7cd7c9f9
Anthony Green <green@moxielogic.com>
2020-07-24 15:05:53 -0400

    fix type= bug

--- rlgl-server.lisp
+++ rlgl-server.lisp
@@ -203,8 +203,8 @@ recognize it, return a RLGL-SERVER:PARSER object, NIL otherwise."
 	(:meta :charset "utf-8")
 	(:meta :name "viewport" :content "width=device-width, initial-scale=1, shrink-to-fit=no")
 	(:link :rel "apple-touch-icon" :sizes "180x180" :href "images/apple-touch-icon.png")
-	(:link :rel "icon" type="image/png" :sizes "32x32" :href "images/favicon-32x32.png")
-	(:link :rel "icon" type="image/png" :sizes "16x16" :href "images/favicon-16x16.png")
+	(:link :rel "icon" :type "image/png" :sizes "32x32" :href "images/favicon-32x32.png")
+	(:link :rel "icon" :type "image/png" :sizes "16x16" :href "images/favicon-16x16.png")
 	(:link :rel "manifest" :href "images/site.webmanifest")
 	(:link :rel "mask-icon" :href "images/safari-pinned-tab.svg" color="#5bbad5")
 	(:meta :name "msapplication-TileColor" content="#da532c")
@@ -353,7 +353,7 @@ recognize it, return a RLGL-SERVER:PARSER object, NIL otherwise."
 				 :crossorigin "anonymous"))
 	   (:script :attrs (emit-bootstrap.min.js)))))
       (let ((redirect-url
-	      (format nil "~A/protocol/openid-connect/auth?client_id=~A&redirect_uri=~A/get-api-key2&response_type=code"
+	      (format nil "~A/protocol/openid-connect/auth?client_id=~A&redirect_uri=~A/get-api-key2&response_:type code"
 		      *keycloak-oidc-realm-uri*
 		      *keycloak-oidc-client-id*
 		      *server-uri*)))
@@ -382,8 +382,8 @@ recognize it, return a RLGL-SERVER:PARSER object, NIL otherwise."
 	    (:meta :charset "utf-8")
 	    (:meta :name "viewport" :content "width=device-width, initial-scale=1, shrink-to-fit=no")
 	    (:link :rel "apple-touch-icon" :sizes "180x180" :href "images/apple-touch-icon.png")
-	    (:link :rel "icon" type="image/png" :sizes "32x32" :href "images/favicon-32x32.png")
-	    (:link :rel "icon" type="image/png" :sizes "16x16" :href "images/favicon-16x16.png")
+	    (:link :rel "icon" :type "image/png" :sizes "32x32" :href "images/favicon-32x32.png")
+	    (:link :rel "icon" :type "image/png" :sizes "16x16" :href "images/favicon-16x16.png")
 	    (:link :rel "manifest" :href "images/site.webmanifest")
 	    (:link :rel "mask-icon" :href "images/safari-pinned-tab.svg" color="#5bbad5")
 	    (:meta :name "msapplication-TileColor" content="#da532c")
@@ -557,8 +557,8 @@ recognize it, return a RLGL-SERVER:PARSER object, NIL otherwise."
 	(:meta :charset "utf-8")
 	(:meta :name "viewport" :content "width=device-width, initial-scale=1, shrink-to-fit=no")
 	(:link :rel "apple-touch-icon" :sizes "180x180" :href "images/apple-touch-icon.png")
-	(:link :rel "icon" type="image/png" :sizes "32x32" :href "images/favicon-32x32.png")
-	(:link :rel "icon" type="image/png" :sizes "16x16" :href "images/favicon-16x16.png")
+	(:link :rel "icon" :type "image/png" :sizes "32x32" :href "images/favicon-32x32.png")
+	(:link :rel "icon" :type "image/png" :sizes "16x16" :href "images/favicon-16x16.png")
 	(:link :rel "manifest" :href "images/site.webmanifest")
 	(:link :rel "mask-icon" :href "images/safari-pinned-tab.svg" color="#5bbad5")
 	(:meta :name "msapplication-TileColor" content="#da532c")
