7935ec78d3c4c417444a3356f25d8f305b75ecc0
Anthony Green <green@moxielogic.com>
2021-07-11 09:02:44 -0400

    Use share util funcitons

--- storage-s3.lisp
+++ storage-s3.lisp
@@ -1,7 +1,7 @@
 ;;; -*- Mode: LISP; Syntax: COMMON-LISP; Package: RLGL-SERVER; Base: 10 -*-
 ;;;
-;;; Copyright (C) 2019  Anthony Green <green@moxielogic.com>
-;;;                         
+;;; Copyright (C) 2019, 2021  Anthony Green <green@moxielogic.com>
+;;;
 ;;; This program is free software: you can redistribute it and/or
 ;;; modify it under the terms of the GNU Affero General Public License
 ;;; as published by the Free Software Foundation, either version 3 of
@@ -28,21 +28,6 @@
 	(uiop:getenv "AWS_SECRET_KEY")))
 
 ;; ----------------------------------------------------------------------------
-;; Utility functions for random filenames...
-
-(defvar *name-random-state* (make-random-state t))
-(defvar *create-file-name-lock*
-  (bordeaux-threads:make-lock "Temporary File Name Creation Lock"))
-
-(defmacro with-file-name-lock-held (() &body body)
-  `(bordeaux-threads:with-lock-held (*create-file-name-lock*)
-     ,@body))
-
-(defun generate-random-string ()
-  (with-file-name-lock-held ()
-    (format nil "~:@(~36,8,'0R~)" (random (expt 36 8) *name-random-state*))))
-
-;; ----------------------------------------------------------------------------
 
 (defclass storage/s3 (storage-backend)
   ((s3-endpoint       :initarg :s3-endpoint    :reader s3-endpoint)
@@ -65,7 +50,6 @@
 
 (defmethod store-document ((backend storage/s3) document)
   "Store a document into s3 storage."
-  (let ((ref (format nil "RLGL-~A"
-		     (generate-random-string))))
+  (let ((ref (concatenate 'string "RLGL-" (rlgl-util:random-hex-string))))
     (zs3:put-vector document (s3-bucket backend) ref)
     ref))
