d03b707c8cec4b2e28517fdd035d802bedfabb99
Anthony Green <green@moxielogic.com>
2021-07-28 15:00:51 -0400

    Test rekor-cli

--- .ci/test.sh
+++ .ci/test.sh
@@ -8,8 +8,10 @@ cd test
 curl -s http://${1}/cli/rlgl-linux-amd64.tgz | tar --strip-components=2 -xvzf - ./rlgl/rlgl
 curl -s https://github.com/sigstore/rekor/releases/download/v0.3.0/rekor-cli-linux-amd64 > rekor-cli
 chmod +x rekor-cli
+./rekor-cli version
 
 export PATH=$(pwd):$PATH
+rekor-cli version
 
 rlgl login --key=AAAAAAA-BBBBBBB-CCCCCCC-DDDDDDD http://${1}
 ID=$(rlgl start ${1})
