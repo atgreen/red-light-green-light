61eace80227bf5ab25ce77fdd29a7dee1537af01
Anthony Green <green@moxielogic.com>
2021-07-28 14:00:28 -0400

    Install rekor-cli

--- .ci/test.sh
+++ .ci/test.sh
@@ -3,14 +3,19 @@
 set -ex
 
 cd test
-curl -s http://${1}/cli/rlgl-linux-amd64.tgz | \
-    tar --strip-components=2 -xvzf - ./rlgl/rlgl
 
-./rlgl login --key=AAAAAAA-BBBBBBB-CCCCCCC-DDDDDDD http://${1}
-ID=$(./rlgl start ${1})
+# --- Install rlgl and rekor --------------------------------------------------
+curl -s http://${1}/cli/rlgl-linux-amd64.tgz | tar --strip-components=2 -xvzf - ./rlgl/rlgl
+curl -s https://github.com/sigstore/rekor/releases/download/v0.3.0/rekor-cli-linux-amd64 > rekor-cli
+chmod +x rekor-cli
 
-OUT=$(./rlgl e --id=$ID --policy=https://github.com/atgreen/test-policy ./report.html || true)
+export PATH=$(pwd):$PATH
+
+rlgl login --key=AAAAAAA-BBBBBBB-CCCCCCC-DDDDDDD http://${1}
+ID=$(rlgl start ${1})
+
+OUT=$(rlgl e --id=$ID --policy=https://github.com/atgreen/test-policy ./report.html || true)
 echo $OUT
 REPORT=$(echo $OUT | awk '{ print $2 }' | cut -f2 -d=)
-./rlgl verify $REPORT
-./rlgl verify $REPORT | RLGL_CLIENT_PUBKEY=~/.config/rlgl/public_key.pem sh
+rlgl verify $REPORT
+rlgl verify $REPORT | RLGL_CLIENT_PUBKEY=~/.config/rlgl/public_key.pem sh
